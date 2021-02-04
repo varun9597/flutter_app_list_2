553ab3b7ee306ca12ab1fa581932a0538ef166fb
Varun Panditpautra <panditpautrav@gmail.com>
2021-02-04 19:44:10 +0530

    Added comments for better understanding

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,20 @@ import 'package:flutter_app_list/controllers/bindings/authBinding.dart';
 import 'package:flutter_app_list/utils/root.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized(); //Initialize Firebase Application
+  await Firebase
+      .initializeApp(); // This needs to be done whenever Firebase is used.
+  runApp(MyApp()); //Points to the below MyApp Class
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      initialBinding: AuthBinding(),
-      home: Root(),
+      //GetMaterialApp is used when we us GetX for navigation
+      initialBinding:
+          AuthBinding(), //initialBinding is used to load the AuthBinding() function when the app starts to improve performance
+      home: Root(), //Points to Root() method in root.dart
       theme: ThemeData.dark(),
     );
   }
--- lib/utils/root.dart
+++ lib/utils/root.dart
@@ -6,14 +6,20 @@ import 'package:flutter_app_list/screens/home.dart';
 import 'package:flutter_app_list/screens/login.dart';
 
 class Root extends GetWidget<AuthController> {
+  //GetWidget is used to cache a controller
   @override
   Widget build(BuildContext context) {
+    //BuildContext is a handle to the location of a widget in a widget tree
     return GetX(
+      //GetX is a reactive state manager
       initState: (_) async {
-        Get.put<UserController>(UserController());
+        // (_) is used when the context argument in the builder is not used
+        Get.put<UserController>(
+            UserController()); // here, we instantiate the class using get.put() so that it is available for all child routes
       },
       builder: (_) {
         if (Get.find<AuthController>().user?.uid != null) {
+          //Get.find is used to find a controller that is being used by another page and redirect you to it
           return Home();
         } else {
           return Login();
